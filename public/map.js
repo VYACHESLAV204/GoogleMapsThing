0f1855528de8f4d2468d202368d636efdd04edfa
Hron0 <sasha1254370@gmail.com>
2024-03-25 17:03:46 +0300

    MapBoxAPI

--- public/map.js
+++ public/map.js
@@ -1,84 +1,188 @@
 //@ts-nocheck
 /* Да, всё этого пиздец как плохо выглядит */
 let map;
-let totalDistance
-let totalTime
-let start_cords
-let end_cords
-const panel = document.getElementById("panel")
-const start_point = document.getElementById("start_point")
-const end_point = document.getElementById("end_point")
-const submitBtn = document.getElementById("submit")
-let modal = document.getElementById("modal")
-let close_modal = document.getElementById("close_modal")
-let submit_modal = document.getElementById("submit_modal")
 
-let start = { lat: 56.82919264514929, lng: 60.58379842794948 };
-let end = { lat: 56.82953942977743, lng: 60.59075440792031 };
+const initMap = () => {
+  const ACCESS_TOKEN = 'pk.eyJ1IjoiYWxleGFuZGVybWFya292IiwiYSI6ImNsdTVibG8zNTB1cDIyam40Y3Nnc2JibTgifQ.-PRze3fjfRYyKtJcXcpJPQ';
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGFuZGVybWFya292IiwiYSI6ImNsdTVibG8zNTB1cDIyam40Y3Nnc2JibTgifQ.-PRze3fjfRYyKtJcXcpJPQ';
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [30.313572325520312, 59.936930470787075], // starting position
+    zoom: 16
+  });
 
-// TO MAKE THE MAP APPEAR YOU MUST
-// ADD YOUR ACCESS TOKEN FROM
-// https://account.mapbox.com
-mapboxgl.accessToken = 'sk.eyJ1IjoiYWxleGFuZGVybWFya292IiwiYSI6ImNsdTVicTdkbzB1cXIyam40bzh4YWZwZHIifQ.VtIzClpn_JOhRlDc1R8EZg';
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v9',
-  projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
-  zoom: 1,
-  center: [30, 15]
-});
 
-map.addControl(new mapboxgl.NavigationControl());
-map.scrollZoom.disable();
+  /* Поисковик */
+  const searchJS = document.getElementById('search-js');
+  searchJS.onload = function () {
+    const searchBox = new MapboxSearchBox();
+    searchBox.accessToken = ACCESS_TOKEN;
+    searchBox.options = {
+      types: 'address,poi,street,place,block',
+      county: 'ru',
+    };
+    searchBox.marker = true;
+    searchBox.mapboxgl = mapboxgl;
+    map.addControl(searchBox);
+  };
 
-map.on('style.load', () => {
-  map.setFog({}); // Set the default atmosphere style
-});
+  /* Маркер с инфой */
+  var popup = new mapboxgl.Popup({ offset: 25 })
+    .setText("Для размещения маршрута нажимайте ЛКМ размещая точки последовательно, после нажмите на последнюю точку. Это создаст маршрут. Нажав на опорную точку выберется весь маршрут, в этом состояние можно перетаскивать любую из точек, корректируя маршрут. Также маршрут можно удалить, выбрав его и нажав кнопку в Верхнем Правом углу. Сверху справа так же есть Поисковик и Кнопки: Создания нового маршрута, Удаления Выбранного маршрута. После того как вы сделали маршрут, нажмите кнопку \"Отправить данные\"")
 
-// The following values can be changed to control rotation speed:
+  var marker = new mapboxgl.Marker()
+    .setLngLat([30.313572325520312, 59.936930470787075]) // replace with your coordinates
+    .setPopup(popup)
+    .addTo(map)
+    .togglePopup();
 
-// At low zooms, complete a revolution every two minutes.
-const secondsPerRevolution = 240;
-// Above zoom level 5, do not rotate.
-const maxSpinZoom = 5;
-// Rotate at intermediate speeds between zoom levels 3 and 5.
-const slowSpinZoom = 3;
 
-let userInteracting = false;
-const spinEnabled = true;
+  /* Рисование (без путей) */
+  const draw = new MapboxDraw({
+    // Instead of showing all the draw tools, show only the line string and delete tools.
+    displayControlsDefault: false,
+    controls: {
+      line_string: true,
+      trash: true
+    },
+    // Set the draw mode to draw LineStrings by default.
+    defaultMode: 'draw_line_string',
+    styles: [
+      // Set the line style for the user-input coordinates.
+      {
+        id: 'gl-draw-line',
+        type: 'line',
+        filter: ['all', ['==', '$type', 'LineString'], ['!=', 'mode', 'static']],
+        layout: {
+          'line-cap': 'round',
+          'line-join': 'round'
+        },
+        paint: {
+          'line-color': '#438EE4',
+          'line-dasharray': [0.2, 2],
+          'line-width': 4,
+          'line-opacity': 0.7
+        }
+      },
+      // Style the vertex point halos.
+      {
+        id: 'gl-draw-polygon-and-line-vertex-halo-active',
+        type: 'circle',
+        filter: [
+          'all',
+          ['==', 'meta', 'vertex'],
+          ['==', '$type', 'Point'],
+          ['!=', 'mode', 'static']
+        ],
+        paint: {
+          'circle-radius': 12,
+          'circle-color': '#FFF'
+        }
+      },
+      // Style the vertex points.
+      {
+        id: 'gl-draw-polygon-and-line-vertex-active',
+        type: 'circle',
+        filter: [
+          'all',
+          ['==', 'meta', 'vertex'],
+          ['==', '$type', 'Point'],
+          ['!=', 'mode', 'static']
+        ],
+        paint: {
+          'circle-radius': 8,
+          'circle-color': '#438EE4'
+        }
+      }
+    ]
+  });
+  map.addControl(draw);
 
-function spinGlobe() {
-  const zoom = map.getZoom();
-  if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
-    let distancePerSecond = 360 / secondsPerRevolution;
-    if (zoom > slowSpinZoom) {
-      // Slow spinning at higher zooms
-      const zoomDif =
-        (maxSpinZoom - zoom) / (maxSpinZoom - slowSpinZoom);
-      distancePerSecond *= zoomDif;
+
+  /* Запрос на отрисовку путей */
+  function addRoute(coords) {
+    if (map.getSource('route')) {
+      map.removeLayer('route');
+      map.removeSource('route');
+    } else {
+      map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'Feature',
+            properties: {},
+            geometry: coords
+          }
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#03AA46',
+          'line-width': 8,
+          'line-opacity': 0.8
+        }
+      });
     }
-    const center = map.getCenter();
-    center.lng -= distancePerSecond;
-    // Smoothly animate the map over one second.
-    // When this animation is complete, it calls a 'moveend' event.
-    map.easeTo({ center, duration: 1000, easing: (n) => n });
   }
+
+
+  function updateRoute() {
+    const profile = 'driving';
+    const data = draw.getAll();
+    const lastFeature = data.features.length - 1;
+    const coords = data.features[lastFeature].geometry.coordinates;
+    const newCoords = coords.join(';');
+    const radius = coords.map(() => 25);
+
+    getMatch(newCoords, radius, profile);
+  }
+
+
+
+  async function getMatch(coordinates, radius, profile) {
+    const radiuses = radius.join(';');
+    const query = await fetch(
+      `https://api.mapbox.com/matching/v5/mapbox/${profile}/${coordinates}?geometries=geojson&radiuses=${radiuses}&steps=true&access_token=${mapboxgl.accessToken}`,
+      { method: 'GET' }
+    );
+    const response = await query.json();
+    if (response.code !== 'Ok') {
+      alert(
+        `${response.code} - ${response.message}.\n\nFor more information: https://docs.mapbox.com/api/navigation/map-matching/#map-matching-api-errors`
+      );
+      return;
+    }
+    const coords = response.matchings[0].geometry;
+    addRoute(coords);
+  }
+
+
+  /* Удаление всех путей */
+  function removeRoute() {
+    if (!map.getSource('route')) return;
+    map.removeLayer('route');
+    map.removeSource('route');
+  }
+
+  map.on('draw.delete', removeRoute);
+  map.on('draw.create', updateRoute);
+  map.on('draw.update', updateRoute);
+
+
+  /* const addMarker = (e) => {
+    console.log(e)
+  }
+
+  map.on('click', addMarker) */
+
 }
 
-// Pause spinning on interaction
-map.on('mousedown', () => {
-  userInteracting = true;
-});
-map.on('dragstart', () => {
-  userInteracting = true;
-});
-
-// When animation is complete, start spinning if there is no ongoing interaction
-map.on('moveend', () => {
-  spinGlobe();
-});
-
-
+initMap()
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer, start, end) {
 
@@ -127,9 +231,6 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer, start, 
   })
 }
 
-close_modal.addEventListener("click", (e) => {
-  modal.classList.remove("active")
-})
 
 function dataProcessing(res) {
   if (res.status !== "OK") {
